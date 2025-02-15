f30a3a6483515de27c06d08460ea93326b6a915f
DhanasekaranSP <dspdhana06@gmail.com>
2025-02-16 03:40:52 +0530

    Deployment

--- email-generator-ui/src/App.tsx
+++ email-generator-ui/src/App.tsx
@@ -25,7 +25,7 @@ function App() {
     setLoading(true);
     setError('');
     try {
-      const response = await axios.post("http://localhost:8080/api/email/generate", {
+      const response = await axios.post(`${import.meta.env.VITE_APP_API_URL}/api/email/generate`, {
         emailContent,
         tone
       });
